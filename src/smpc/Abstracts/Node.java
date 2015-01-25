4cb0e34f170a1fbeb3d2ca87dd5f5252ed9baf49
fardin abdi <abditag2@illinois.edu>
2015-01-25 17:32:22 -0600

    node updated

--- src/smpc/Abstracts/Node.java
+++ src/smpc/Abstracts/Node.java
@@ -2,6 +2,8 @@ package smpc.Abstracts;
 
 import java.util.LinkedList;
 
+import smpc.Config;
+
 public class Node {
 	
 	
@@ -34,18 +36,47 @@ public class Node {
 		 *the goal of this function is to schedule the packets until the end of cycle and
 		 *set the communication flows that are completely recieved. 
 		 */
-
 		
+		int now = currentTime;
+		int nextstopTime = findMinStartEndtimesAfterCurrentTime(
+				now, 
+				packetQToBeRecieved);
 		
+		for (NetworkPacket packet:packetQToBeRecieved) {
+			
+			
+			
+			if (now >= packet.startTime){
+				packet.numberOfRemainingPackets - 
+			}
+		}
+   	}
 	
-		
-	}
-	
-	public int findMinStartEndtimesAfterCurrentTime(
-			int currentTime, 
+	public int[] findMinStartEndtimesAfterCurrentTime(
+			int now, 
 			LinkedList<NetworkPacket> packetQToBeRecieved)
 	{
+		int minTime = Integer.MAX_VALUE;
+		int minPacket = Integer.MAX_VALUE;
 		
-		return min;
+		int countOngoingFlows = 0 ; 
+		for (NetworkPacket packet:packetQToBeRecieved) {
+			//find the minimum number of packets remaining for the nodes in between the flow
+			if (now >= packet.startTime) {
+				countOngoingFlows++;
+				if (minPacket < packet.numberOfRemainingPackets)
+					minPacket = packet.numberOfRemainingPackets;
+			}
+			
+			else if (now <= packet.startTime) {
+				if (minTime > packet.startTime)
+					minTime = packet.startTime;
+			}			
+		}
+		
+		int rate = Config.nodeIncomingBandWidth;
+		int minTimeForOngoingFlows = minPacket/rate + 1; //+1 is for the division error
+		int nextTime = (int) Math.min(minTime, minTimeForOngoingFlows);
+		return new int[] {nextTime, countOngoingFlows};
 	}
 }
